9bce9aa4e2bac4fa0d90b182c94ded4d6c7c9e01
yakexerr <senya.evseev.05@mail.ru>
2025-02-01 12:38:08 +0300

    fdf

--- ЦК/2/2.3.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-# узнаём информацию о системе
-print(sys.version)
-# узнаём информацию о системе в более подробном формате
-print(sys.version_info)
---
+++ ЦК/2/source_dir/2_3.py
@@ -0,0 +1,12 @@
+import sys
+
+
+print(sys.version) # узнаём информацию о системе
+
+print(sys.version_info) # узнаём информацию о системе в более подробном формате
+
+
+source_dir = sys.argv[1]
+dest_dir = sys.argv[2]
+print(source_dir)
+print(dest_dir)
